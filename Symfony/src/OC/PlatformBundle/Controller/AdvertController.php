25a41267ee06af71c235f5056b73d02c6dff9232
Abel L. MBULA <bam92@debian>
2017-05-26 06:43:28 +0200

    Templates added

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -13,13 +13,56 @@ class AdvertController extends Controller
 {
 	
 	public function indexAction($page)
-	{
-		if ($page < 1) {
-			throw new NotFoundHttpException('Page "' .$page. '" inexistante.');
-			
-		}
-		
-		return $this->render('OCPlatformBundle:Advert:index.html.twig');
+	{// Notre liste d'annonce en dur
+
+    $listAdverts = array(
+
+      array(
+
+        'title'   => 'Recherche développpeur Symfony',
+
+        'id'      => 1,
+
+        'author'  => 'Alexandre',
+
+        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+
+        'date'    => new \Datetime()),
+
+      array(
+
+        'title'   => 'Mission de webmaster',
+
+        'id'      => 2,
+
+        'author'  => 'Hugo',
+
+        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+
+        'date'    => new \Datetime()),
+
+      array(
+
+        'title'   => 'Offre de stage webdesigner',
+
+        'id'      => 3,
+
+        'author'  => 'Mathieu',
+
+        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+
+        'date'    => new \Datetime())
+
+    );
+
+
+    // Et modifiez le 2nd argument pour injecter notre liste
+
+    return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+
+      'listAdverts' => $listAdverts
+
+    ));
 		
 	}
 
@@ -60,4 +103,23 @@ class AdvertController extends Controller
     {        
         return $this->render('OCPlatformBundle:Advert:delete.html.twig');
     }
+
+    public function menuAction()
+    {
+      // On fixe en dur une liste ici, bien entendu par la suite
+     // on la récupérera depuis la BDD !
+     $listAdverts = array(
+        array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+        array('id' => 5, 'title' => 'Mission de webmaster'),
+        array('id' => 9, 'title' => 'Offre de stage webdesigner')
+      );
+
+     return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+
+      // Tout l'intérêt est ici : le contrôleur passe
+      // les variables nécessaires au template !
+      'listAdverts' => $listAdverts
+    ));
+
+  }
 }
